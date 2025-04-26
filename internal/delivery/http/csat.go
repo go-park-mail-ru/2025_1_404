5689664e3da2e3dea4795b878548c315cd07812c
Maksim shabalin <sabalin291@gmail.com>
2025-04-26 14:55:07 +0300

    fix get question by event

--- internal/delivery/http/csat.go
+++ internal/delivery/http/csat.go
@@ -17,16 +17,16 @@ func NewCsatHandler (uc csatUsecase) *CsatHandler {
 }
 
 func (h *CsatHandler) GetQuestionsByEvent (w http.ResponseWriter, r *http.Request) {
-	event := r.URL.Query().Get("event")
+	event := r.URL.Query().Get("event_name")
 	if event == "" {
-		utils.SendErrorResponse(w, "нет параметра event", http.StatusBadRequest)
+		utils.SendErrorResponse(w, "нет параметра event_name", http.StatusBadRequest)
 		return
 	}
 
 	questions, err := h.UC.GetQuestionsByEvent(r.Context(), event)
 	
 	if err != nil {
-		utils.SendErrorResponse(w, "event не найден", http.StatusBadRequest)
+		utils.SendErrorResponse(w, "event_name не найден", http.StatusBadRequest)
 		return
 	}
 
@@ -46,7 +46,7 @@ func (h *CsatHandler) AddAnswerToQuestion (w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	utils.SendJSONResponse(w, nil, http.StatusCreated)
+	utils.SendJSONResponse(w, nil, http.StatusOK)
 }
 
 func (h *CsatHandler) GetAnswersByQuestion (w http.ResponseWriter, r *http.Request) {
